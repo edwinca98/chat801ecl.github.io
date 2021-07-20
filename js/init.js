7f05fab27b2d6c55645d51c854ab66ae721d19fb
edwinca98 <87714379+edwinca98@users.noreply.github.com>
2021-07-20 15:51:06 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyANQUXdsIkPzPxfhP-nEuRDy42Q1eU3ju4",
+    authDomain: "chat801edca.firebaseapp.com",
+    projectId: "chat801edca",
+    storageBucket: "chat801edca.appspot.com",
+    messagingSenderId: "462664837081",
+    appId: "1:462664837081:web:3a64650eae478ee3651bc8",
+    measurementId: "G-95HN2J5YT7"
+    });
